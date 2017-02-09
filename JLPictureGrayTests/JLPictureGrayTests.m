df9cfdde31b03dc4ad1d769e722614cabd7fa6ee
afyylong <afyylong@afyylongdemacbook-pro.local>
2017-02-09 19:07:56 +0800

    Initial Commit

---
+++ JLPictureGrayTests/JLPictureGrayTests.m
@@ -0,0 +1,39 @@
+//
+//  JLPictureGrayTests.m
+//  JLPictureGrayTests
+//
+//  Created by afyylong on 2017/2/9.
+//  Copyright © 2017年 afyylong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JLPictureGrayTests : XCTestCase
+
+@end
+
+@implementation JLPictureGrayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
